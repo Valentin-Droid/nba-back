8ef7f87fb882fbe261501888814293484a402eb5
Valentin Drelon <63446112+Valentin-Droid@users.noreply.github.com>
2024-07-01 09:46:16 +0200

    :bug: correction connection to api while deployed on vercel

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ import { corsMiddleware } from './middlewares.js';
 import 'dotenv/config';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(corsMiddleware);
+
+// Route racine pour vérification rapide
+app.get('/', (req, res) => {
+  res.send('NBA API is running!');
+});
+
 app.use('/nba', nbaRouter);
 
+// Gestion des erreurs globale
+app.use((err, req, res) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
