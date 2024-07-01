5f026a0251b19d7621dbae13c388882e61d75623
Valentin Drelon <63446112+Valentin-Droid@users.noreply.github.com>
2024-07-01 14:41:14 +0200

    :bug: change api url

--- middlewares.js
+++ middlewares.js
@@ -1,7 +1,20 @@
 import cors from 'cors';
 
+const allowedOrigins = [
+  'http://localhost:3001',
+  'https://nba-front-ten.vercel.app',
+];
+
 export const corsOptions = {
-  origin: 'http://localhost:3001',
+  origin: function (origin, callback) {
+    // Permettre les requêtes sans origine (comme les appels d'API mobile)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   optionsSuccessStatus: 200,
 };
 
